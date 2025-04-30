ed2caff5d87c39c3686feeaff821954fbcbbab57
cetk38 <samet383800@gmail.com>
2025-04-30 12:39:42 +0300

    Açıklama satırı eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
+// ===========================================================
+// Erciyes Üniversitesi - Mühendislik Fakültesi
+// Bilgisayar Mühendisliği Bölümü
+// Dersi: MOBILE APPLICATION DEVELOPMENT
+// Öğretim Üyesi: [Dr. Öğr. FEHİM KÖYLÜ]
+// Öğrenci Adı: [SAMET TOK]
+// Öğrenci Numarası: [1030521081]
+// Proje Türü: PROJE ÖDEVİ 
+// Açıklama: Flutter ile SQLite tabanlı giriş sistemi, admin paneli ve kullanıcı ekranı içeren mobil uygulama.
+// ===========================================================
+
 import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
 
